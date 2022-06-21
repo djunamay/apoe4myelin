5f87359f95f3b2aed6d074f5331c8d62779283ba
Djuna <djunavm@gmail.com>
2022-06-21 11:31:56 -0400

    simplifying the pathway analysis

--- scripts/pathway_analyses.r
+++ scripts/pathway_analyses.r
@@ -14,112 +14,39 @@ set.seed(5)
 
 # load the pathways
 print('loading pathways')
-pathways = readRDS('../data_outputs/pathways.rds')
+pathways = readRDS('../data/other_analyses_outputs/pathways.rds')
 low_removed_bp = pathways$pathways$low_removed_bp
 apoe_gsets_low_removed = pathways$pathways$apoe_gsets_low_removed
 apoe_gsets = pathways$pathways$apoe_gsets_all
 all_paths = pathways$pathways$all
 low_removed_lipid_paths = pathways$pathways$low_removed_lipid_associated
 
-# output data
+# load the data
+print('loading the data..')
+expressed = readRDS('../data/single_cell_data/expressed_genes_per_celltype.rds')
+av_expression = readRDS('../data/single_cell_data/individual_level_averages_per_celltype.rds')
+summary = read.csv('../data/single_cell_data/metadata_by_individual.csv')
+summary$APOE4 = ifelse(summary$apoe_genotype == '33','e3','e4')
+rownames(summary) = summary[,'projid...2']
+
 all_data = list()
 
-# load the average expression data and metadata
-print('loading data') # TODO simplify data
-e = readRDS('../../submission_code_09012021/data/Summary.DE.celltype.rds')
-expressed = metadata(e)$expressed.genes
-av_expression = readRDS('../../submission_code_09012021/data/Averages.by.celltype.by.individual.rds')
-
-summary = as.data.frame(read_excel('../../submission_code_09012021/data/Overview_PFC_projids_data_384.xlsx'))
-summary = summary[!duplicated(summary[,'projid...2']),]
-rownames(summary) = summary[['projid...2']]
-
-##############################
-###### GO BP PATHWAYS #####
-##############################
-print('analysis on GO BP pathways...')
-# run GSVA on GO pathways
-print('running GSVA...')
+# perform enrichment analyses for different pathway databases
+print('performing enrichment analyses...')
 order = c('In', 'Ex', 'Oli', 'Opc', 'Mic','Ast')
-out_bp_terms = lapply(order, function(x) t(gsva(as.matrix(av_expression[[x]]), low_removed_bp[[x]], mx.diff=TRUE, verbose=F, kcdf=c("Gaussian"), min.sz=5, max.sz = 150, parallel.sz=16)))
-names(out_bp_terms) = order
-all_data[['res']][['BP']][['gsva_out']] = out_bp_terms
+print('GO BP...')
+out_bp = get_pathway_fits(order, av_expression, low_removed_bp, top_20=TRUE, summary)
+all_data[['GO_BP']] = out_bp
 
-# get linear model fits
-print('getting linear model fits...')
-summary$APOE4 = ifelse(summary$apoe_genotype == '33','e3','e4')
-fits = get_fits(out_bp_terms, summary)
-all_data[['res']][['BP']][['fits']] = fits
+print('APOE...')
+out_apoe = get_pathway_fits(order, av_expression, apoe_gsets_low_removed, top_20=FALSE, summary)
+all_data[['APOE']] = out_apoe
 
-# get matrix of scores for heatmap
-print('visualizing scores as heatmap...')
-scores = get_scores(fits)
-all_data[['res']][['GO_BP']][['scores']] = scores
-names = unique(unname(unlist(lapply(names(scores$all), function(x) rownames(scores$all[[x]])))))
-mat = get_matrix(scores$all, names)
-mat = mat[unname(rowSums(abs(mat)>1.3)>0),]
-index = unique(unname(unlist(lapply(colnames(mat), function(x) order(abs(mat[[x]]),decreasing = T)[1:20]))))
-mat = mat[index,]
-all_data[['res']][['GO_BP']][['scores_top_20']] = mat
-
-#####################################
-###### APOE-ASSOCIATED PATHWAYS #####
-#####################################
-print('analysis on APOE-associated pathways...')
-# run GSVA on APOE-associated pathways
-print('running GSVA...')
-order = c('In', 'Ex', 'Oli', 'Opc', 'Mic','Ast')
-out_apoe_terms = lapply(order, function(x) t(gsva(as.matrix(av_expression[[x]]), apoe_gsets_low_removed[[x]], mx.diff=TRUE, verbose=F, kcdf=c("Gaussian"), min.sz=5, max.sz = 150, parallel.sz=16)))
-names(out_apoe_terms) = order
-all_data[['res']][['apoe_associated']][['gsva_out']] = out_apoe_terms
-
-# get linear model fits
-print('getting linear model fits...')
-summary$APOE4 = ifelse(summary$apoe_genotype == '33','e3','e4')
-fits = get_fits(out_apoe_terms, summary) # TODO make the trace more informative
-all_data[['res']][['apoe_associated']][['fits']] = fits
-
-# visualize scores as heatmap
-print('getting scores...')
-scores = get_scores(fits)
-all_data[['res']][['apoe_associated']][['scores']] = scores
-
-# show the heatmap for the APOE-associated pathways
-print('drawing the apoe-associated heatmap...')
-names = unique(unname(unlist(lapply(names(scores$all), function(x) rownames(scores$all[[x]])))))
-mat = get_matrix(scores$all, names)
-df = mat[unname(rowSums(abs(mat)>1.3)>0),]
-all_data[['res']][['apoe_associated']][['scores_sig']] = df
-
-#####################################
-###### LIPID-ASSOCIATED PATHWAYS #####
-#####################################
-print('analysis on lipid-associated pathways...')
-# run GSVA on reactome pathways
-print('running GSVA...')
-order = c('In', 'Ex', 'Oli', 'Opc', 'Mic','Ast')
-out_lipid_terms = lapply(order, function(x) t(gsva(as.matrix(av_expression[[x]]), low_removed_lipid_paths[[x]], mx.diff=TRUE, verbose=F, kcdf=c("Gaussian"), min.sz=5, max.sz = 150, parallel.sz=16)))
-names(out_lipid_terms) = order
-all_data[['res']][['lipid_associated']][['gsva_out']] = out_lipid_terms
-
-# get linear model fits
-print('getting linear model fits...')
-summary$APOE4 = ifelse(summary$apoe_genotype == '33','e3','e4')
-fits = get_fits(out_lipid_terms, summary)
-all_data[['res']][['lipid_associated']][['fits']] = fits
-
-# visualize scores as heatmap
-print('getting scores...')
-scores = get_scores(fits)
-all_data[['res']][['lipid_associated']][['scores']] = scores
-
-# show the heatmap
-print('drawing the apoe-associated heatmap...')
-names = unique(unname(unlist(lapply(names(scores$all), function(x) rownames(scores$all[[x]])))))
-mat = get_matrix(scores$all, names)
-df = mat[unname(rowSums(abs(mat)>1.3)>0),]
-all_data[['res']][['lipid_associated']][['scores_sig']] = df
+print('lipid...')
+out_lipid = get_pathway_fits(order, av_expression, low_removed_lipid_paths, top_20=FALSE, summary)
+all_data[['lipid']] = out_lipid
 
 # save all the data
-saveRDS(all_data, '../data_outputs/pathway_scores.rds')
+print('saving the data...')
+saveRDS(all_data, '../data/other_analyses_outputs/pathway_scores.rds')
 print('done.')
