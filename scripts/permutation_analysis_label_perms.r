73d9880c44eb80bb33b8b060a8afb9cc4483ea14
Djuna <djunavm@gmail.com>
2022-06-21 15:13:03 -0400

    cleaning up

--- scripts/permutation_analysis_label_perms.r
+++ scripts/permutation_analysis_label_perms.r
@@ -8,37 +8,35 @@ library('parallel')
 library('tidyr')
 library('stringr')
 library('pbmcapply')
+library(ggplot2)
 set.seed(5)
 
 # load observed pathway scores
-scores = readRDS('../data_outputs/pathway_scores.rds')
+scores = readRDS('../data/other_analyses_outputs/pathway_scores.rds')
 paths = list()
 medians = list()
 for(i in c('Oli', 'Opc', 'Ex', 'In', 'Ast', 'Mic')){
-  df = (scores$res$apoe_associated$scores$all[[i]])
+  df = (scores$APOE$scores_all$all[[i]])
   names = df[df$P.Value<0.05,'names']
   medians[[i]] = median((df[df$P.Value<0.05,'logFC']))
   paths[[i]] = names
 }
 
-
 # load the pathways
 print('loading pathways')
-pathways = readRDS('../data_outputs/pathways.rds')
+pathways = readRDS('../data/other_analyses_outputs/pathways.rds')
 apoe_gsets_low_removed = pathways$pathways$apoe_gsets_low_removed
 
 # load the average expression data and metadata
-print('loading data') # TODO simplify data
-e = readRDS('../../submission_code_09012021/data/Summary.DE.celltype.rds')
-expressed = metadata(e)$expressed.genes
-av_expression = readRDS('../../submission_code_09012021/data/Averages.by.celltype.by.individual.rds')
+print('loading the data..')
+expressed = readRDS('../data/single_cell_data/expressed_genes_per_celltype.rds')
+av_expression = readRDS('../data/single_cell_data/individual_level_averages_per_celltype.rds')
+summary = read.csv('../data/single_cell_data/metadata_by_individual.csv')
+summary$APOE4 = ifelse(summary$apoe_genotype == '33','e3','e4')
+rownames(summary) = summary[,'projid...2']
 
-summary = as.data.frame(read_excel('../../submission_code_09012021/data/Overview_PFC_projids_data_384.xlsx'))
-summary = summary[!duplicated(summary[,'projid...2']),]
-rownames(summary) = summary[['projid...2']]
-
-all_data = readRDS('../data_outputs/pathway_scores.rds')
-out_apoe_terms = all_data[['res']][['apoe_associated']][['gsva_out']]
+all_data = readRDS('../data/other_analyses_outputs/pathway_scores.rds')
+out_apoe_terms = all_data$APOE$gsva_out
 summary$APOE4 = ifelse(summary$apoe_genotype == '33','e3','e4')
 
 get_rand_res_permute_labels = function(out_lipid_terms, summary){
@@ -50,17 +48,6 @@ get_rand_res_permute_labels = function(out_lipid_terms, summary){
 }
 
 out = pbmclapply(1:10000, function(x) get_rand_res_permute_labels(out_apoe_terms, summary), mc.cores = 16)
-
-get_rand_res_permute_labels = function(out_lipid_terms, summary){
-    summary$APOE4 = sample(summary$APOE4)
-    fits = get_fits(out_lipid_terms, summary)
-    rand = lapply(names(fits), function(x) median(abs(fits[[x]][fits[[x]]$P.Value<0.05,'logFC'])))
-    names(rand) = names(fits)
-    return(unlist(rand))
-}
-
-out = pbmclapply(1:10000, function(x) get_rand_res_permute_labels(out_apoe_terms, summary), mc.cores = 16)
-
 
 # plot label permutations
 x = do.call('rbind', out)
@@ -85,4 +72,5 @@ for(i in unique(colnames(x))){
   dev.off()
 }
 
-saveRDS(out, '../data_outputs/permutation_analysis_label_perms.rds')
+print('done.')
+saveRDS(out, '../data/other_analyses_outputs/permutation_analysis_label_perms.rds')
