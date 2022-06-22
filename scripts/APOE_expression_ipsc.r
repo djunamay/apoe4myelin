433c48f6be9d8f8e09c07ea4213222c62adfd8ee
Djuna <djunavm@gmail.com>
2022-06-22 09:50:07 -0400

    saving tables

--- scripts/APOE_expression_ipsc.r
+++ scripts/APOE_expression_ipsc.r
@@ -1,8 +1,10 @@
 # get the data
-counts = read.csv('../data_outputs/ipsc_bulk_no_drug.csv')
+library(ggplot2)
 
-meta1 = read.table('../raw_data/APOE_ipsc_opc_sequencing_meta.csv', sep = ',', header = F)
-x = as.data.frame(read.table('../raw_data/191210Tsa/RNAseqQC-4367S/D19-13224-4367S_geneexp.txt', header = FALSE))
+counts = read.csv('../data/iPSC_data/ipsc_bulk_no_drug.csv')
+
+meta1 = read.table('../data/iPSC_data/ipsc_metadata.csv', sep = ',', header = F)
+x = as.data.frame(read.table('../data/iPSC_data/ipsc_bulk_rnaseq_count_files/D19-13224-4367S_geneexp.txt', header = FALSE))
 gene.df = as.data.frame(cbind(as.character(x$V1), as.character(x$V3)))
 rownames(gene.df) = gene.df$V1
 counts$gene = gene.df[counts[,1],'V2']
@@ -19,17 +21,11 @@ df = df[!df$means==0,,drop = F]
 
 print('APOE percentile expressed gene, among nonzero detected genes:')
 d = df[order(df$means,decreasing = F),,drop = F]
-print(which(rownames(d) =='APOE')/nrow(d))
-
-print('fraction of genes captured in the untransformed histogram:')
-x = table(df$means<50)
-print(x[2]/sum(x))
-
-library(ggplot2)
+percentile = (which(rownames(d) =='APOE')/nrow(d))*100
 
 pdf('../plots/apoe_expression_ipsc.pdf', width = 5, height = 5)
 ggplot(df, aes(x = (means))) +
   geom_histogram(aes(y = ..density..),
                  colour = 1, fill = "white", binwidth =0.5 ) +
-  geom_density() + geom_vline(xintercept=(c((df['APOE',1]))), size=.5, color="black")+theme_classic() + xlim(c(0,50))
+  geom_density() + geom_vline(xintercept=(c((df['APOE',1]))), size=.5, color="black")+theme_classic() + xlim(c(0,50)) + annotate("text", x = 4, y=1, label = percentile)
 dev.off()
--- scripts/APOE_expression_oligodendrocytes.r
+++ scripts/APOE_expression_oligodendrocytes.r
@@ -1,7 +1,8 @@
 # population of APOE-expressing oligodendrocytes
+library(ggplot2)
 
 # load sce
-sce = readRDS('../../submission_code_09012021/data/snRNA.data.sce.rds')
+sce = readRDS('../data/single_cell_data/single_cell_experiment_object.rds')
 oli = sce[,sce$cell.type=='Oli']
 
 # get APOE expression
@@ -9,15 +10,11 @@ d = as.data.frame(logcounts(oli)['APOE',])
 colnames(d) = c('APOE')
 
 # how is APOE detected relative to other genes?
-library(ggplot2)
-
 cts = counts(oli)
 cts_binary = cts>0
 sums = rowSums(cts_binary)
 det.rate = sums/ncol(cts_binary)
 df = as.data.frame(det.rate)
-
-options(repr.plot.width = 3, repr.plot.height = 3, repr.plot.res = 300)
 
 print('order of genes=')
 print(c('APOE', 'NEUROD1', 'OLIG2', 'MBP'))
@@ -27,10 +24,8 @@ pdf('../plots/distribution_apoe.pdf', width = 3, height = 3)
 ggplot(df, aes(x = det.rate)) +
   geom_histogram(aes(y = ..density..),
                  colour = 1, fill = "white", binwidth =0.01 ) +
-  geom_density() + geom_vline(xintercept=(c(df['APOE',1], df['NEUROD1',1], df['OLIG2',1],df['MBP',1])), size=.5, color="black")+theme_classic()
+  geom_density() + geom_vline(xintercept=(c(df['APOE',1])), size=.5, color="black")+theme_classic()
 dev.off()
-
-# how large is the population of APOE-detecting Oligodendrocytes
 
 d = as.data.frame(logcounts(oli)['APOE',])
 colnames(d) = c('APOE')
@@ -44,3 +39,13 @@ ggplot(d, aes(x = APOE)) +
                  colour = 1, fill = "white", binwidth =0.1 ) +
   geom_density(aes(fill = clust, alpha = 0.5)) +theme_classic() +facet_wrap( ~ clust, nrow = 3, scales = 'free_y')
 dev.off()
+
+# save the info regarding APOE expression/detection
+df = df[!df[,1]==0,,drop=F]
+df$rank = rank(df[,1])
+percentile = df['APOE','rank']/max(df$rank)
+
+out = c(df['APOE',1],percentile*100, table(clust))
+names(out) = c('APOE_detection_rate', 'APOE_detection_rate_percentile', 'APOE_detected_pop', 'APOE_not_detected_pop')
+write.csv(as.data.frame(out), '../data/supplementary_tables/APOE_expression_post_mortem_oligos.csv')
+print('done.')
