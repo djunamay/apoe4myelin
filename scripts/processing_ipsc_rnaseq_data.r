631267cc28a0ee4b0a80f8bb35db44ebece7e75c
Djuna <djunavm@gmail.com>
2022-06-22 09:26:36 -0400

    cleaning up

--- scripts/processing_ipsc_rnaseq_data.r
+++ scripts/processing_ipsc_rnaseq_data.r
@@ -15,16 +15,17 @@ library("edgeR")
 library("limma")
 
 # load the counts data
-path1 = '../raw_data/191210Tsa/RNAseqQC-4367S'
+path1 = '../data/iPSC_data/ipsc_bulk_rnaseq_count_files'
 file_term = 'geneexp.txt'
 counts_no_drug = load_counts(path1, file_term)
+
 # normalize the counts
 norm_counts_no_drug = normalize_counts(counts_no_drug)
-write.csv(norm_counts_no_drug, '../data_outputs/ipsc_bulk_no_drug.csv')
+write.csv(norm_counts_no_drug, '../data/iPSC_data/ipsc_bulk_no_drug.csv')
 
 # get the DEGs
-meta1 = read.table('../raw_data/APOE_ipsc_opc_sequencing_meta.csv', sep = ',', header = F)
-x = as.data.frame(read.table('../raw_data/191210Tsa/RNAseqQC-4367S/D19-13224-4367S_geneexp.txt', header = FALSE))
+meta1 = read.table('../data/iPSC_data/ipsc_metadata.csv', sep = ',', header = F)
+x = as.data.frame(read.table('../data/iPSC_data/ipsc_bulk_rnaseq_count_files/D19-13224-4367S_geneexp.txt', header = FALSE))
 gene.df = as.data.frame(cbind(as.character(x$V1), as.character(x$V3)))
 rownames(gene.df) = gene.df$V1
 var = as.character(meta1$V2)
@@ -34,4 +35,6 @@ counts_no_drug = counts_no_drug[index1,]
 index2 = unname(rowSums(counts_no_drug>0)>(0.5*ncol(counts_no_drug)))
 counts_no_drug = counts_no_drug[index2,]
 out = RunDiffExprAnalysisLimma(counts_no_drug, var, gene.df)
-write.csv(out, '../data_outputs/apoe3_v_apoe4_degs.csv')
+write.csv(out, '../data/iPSC_data/apoe3_v_apoe4_degs.csv')
+
+print('done.')
