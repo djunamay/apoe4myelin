5645d7b1aad2e311f6eee9431d5e3e578ecd3586
Djuna <djunavm@gmail.com>
2022-07-01 22:37:10 -0400

    cleaning up

--- scripts/pathway_analyses.r
+++ scripts/pathway_analyses.r
@@ -1,8 +1,9 @@
 ########## run pathway analyses (GSVA) ##########
 #################################################
+print('|| running GSVA on pathway databases... ||')
 
 ##### required libraries ####
-source('../functions/pathway_analyses.r')
+source('../functions/pathway_fits.r')
 library('GSVA')
 library("readxl")
 library('SingleCellExperiment')
@@ -10,10 +11,8 @@ library('limma')
 library('parallel')
 library('tidyr')
 library('stringr')
-set.seed(5)
 
 # load the pathways
-print('loading pathways')
 pathways = readRDS('../data/other_analyses_outputs/pathways.rds')
 low_removed_bp = pathways$pathways$low_removed_bp
 apoe_gsets_low_removed = pathways$pathways$apoe_gsets_low_removed
@@ -22,7 +21,6 @@ all_paths = pathways$pathways$all
 low_removed_lipid_paths = pathways$pathways$low_removed_lipid_associated
 
 # load the data
-print('loading the data..')
 expressed = readRDS('../data/single_cell_data/expressed_genes_per_celltype.rds')
 av_expression = readRDS('../data/single_cell_data/individual_level_averages_per_celltype.rds')
 summary = read.csv('../data/single_cell_data/metadata_by_individual.csv')
@@ -32,17 +30,17 @@ rownames(summary) = summary[,'projid...2']
 all_data = list()
 
 # perform enrichment analyses for different pathway databases
-print('performing enrichment analyses...')
+print('for GO BP pathways...')
 order = c('In', 'Ex', 'Oli', 'Opc', 'Mic','Ast')
 print('GO BP...')
 out_bp = get_pathway_fits(order, av_expression, low_removed_bp, top_20=TRUE, summary)
 all_data[['GO_BP']] = out_bp
 
-print('APOE...')
+print('for APOE-associated pathways...')
 out_apoe = get_pathway_fits(order, av_expression, apoe_gsets_low_removed, top_20=FALSE, summary)
 all_data[['APOE']] = out_apoe
 
-print('lipid...')
+print('for lipid-associated pathways...')
 out_lipid = get_pathway_fits(order, av_expression, low_removed_lipid_paths, top_20=FALSE, summary)
 all_data[['lipid']] = out_lipid
 
