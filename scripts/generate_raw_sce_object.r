095797363c5be5bafe334f296e4980654d03f21b
Djuna von Maydell <41922169+djunamay@users.noreply.github.com>
2022-10-27 19:24:20 -0400

    Update generate_raw_sce_object.r

--- scripts/generate_raw_sce_object.r
+++ scripts/generate_raw_sce_object.r
@@ -4,12 +4,15 @@ require(SingleCellExperiment)
 require(Matrix)
 ##############################################################################################################
 # load qc files and generate sce
+print('reading matrix')
 C <- readMM("../data/single_cell_data/raw_counts_data/raw_counts.mtx")
+print('making sce object')
 sce <- SingleCellExperiment(assays=list(counts=C))
 rowMeta <- read.csv("../data/single_cell_data/raw_counts_data/gene_names.csv")
 colMeta <- DataFrame(read.csv("../data/single_cell_data/raw_counts_data/column_metadata.csv", row.names = 1))
 sce@colData <- colMeta
 rowData(sce) <- rowMeta
 ##############################################################################################################
-
+print('saving')
 saveRDS(sce, '../data/single_cell_data/single_cell_experiment_object_raw.rds')
+print('done')
