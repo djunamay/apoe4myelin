830797c277dbc695f0e352703c4624e3ee0c1913
Djuna von Maydell <41922169+djunamay@users.noreply.github.com>
2022-10-30 14:26:15 -0400

    Update get_wilcox_degs.r

--- scripts/get_wilcox_degs.r
+++ scripts/get_wilcox_degs.r
@@ -4,7 +4,7 @@
 print('loading the sce object')
 source('../functions/differential_expression.r')
 
-sce = readRDS('../data/single_cell_data/single_cell_experiment_object.rds')
+sce = readRDS('../data/single_cell_data/single_cell_experiment_object_qced.rds')
 
 # for all
 print('computing degs')
@@ -18,8 +18,8 @@ out_ad = Wilcox.differential(logcounts(oli_ad), ifelse(oli_ad$apoe_genotype==33,
 oli_nonad = oli[,oli$niareagansc%in%c(3,4) & oli$apoe_genotype!=44]
 out_nonad = Wilcox.differential(logcounts(oli_nonad), ifelse(oli_nonad$apoe_genotype==33, 0, 1))
 
-saveRDS(out_all, '../data/differentially_expressed_genes_data/oli_wilcox_results.rds')
-saveRDS(out_ad, '../data/differentially_expressed_genes_data/oli_wilcox_results_AD.rds')
-saveRDS(out_nonad, '../data/differentially_expressed_genes_data/oli_wilcox_results_noAD.rds')
+saveRDS(out_all, '../data/differentially_expressed_genes/oli_wilcox_results.rds')
+saveRDS(out_ad, '../data/differentially_expressed_genes/oli_wilcox_results_AD.rds')
+saveRDS(out_nonad, '../data/differentially_expressed_genes/oli_wilcox_results_noAD.rds')
 
 print('done.')
