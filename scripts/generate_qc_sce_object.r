3ada0b4ecd5ebf253564e409d22e59e96d24f4c6
Djuna von Maydell <41922169+djunamay@users.noreply.github.com>
2022-10-30 14:53:22 -0400

    adding normalization step

--- scripts/generate_qc_sce_object.r
+++ scripts/generate_qc_sce_object.r
@@ -12,6 +12,7 @@ rownames(sce_qc) <- readLines("../data/single_cell_data/qc_counts_data/qc_gene_n
 colMeta <- DataFrame(read.csv("../data/single_cell_data/qc_counts_data/qc_column_metadata.csv", row.names = 1))
 colnames(sce_qc) <- rownames(colMeta)
 sce_qc@colData <- colMeta
+sce =  normalize.default(sce)
 ##############################################################################################################
 # plot cells 2D
 plot(sce_qc@colData$x, sce_qc@colData$y, col=scales::alpha(sce_qc@colData$cell.type.color, sce_qc@colData$connectivity), cex=0.15, axes=F, xlab="", ylab="")
