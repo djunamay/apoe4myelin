9d1b348078e24fe1d6945d9ade6adf114ce965bc
Djuna von Maydell <41922169+djunamay@users.noreply.github.com>
2022-10-30 17:27:37 -0400

    Update er_postmortem_plots.r

--- scripts/er_postmortem_plots.r
+++ scripts/er_postmortem_plots.r
@@ -16,14 +16,14 @@ data = readRDS('../data/other_analyses_outputs/er_stress_results.rds')
 
 # plot the ATF6 pathways
 df = data$ER_stress$ATF6_activity
-pdf('../plots/Extended_8/unfolded_protein_response.pdf', width = 2, height = 3)
+pdf('../plots/Extended_7/unfolded_protein_response.pdf', width = 2, height = 3)
 p <- ggplot(df, aes(x=APOE, y=value)) +
   geom_boxplot()
 p + geom_jitter(shape=16, position=position_jitter(0.2)) + stat_compare_means(method = "wilcox.test")
 dev.off()
 
 x = data$ER_stress$ATF6_pathway_degs
-pdf('../plots/Extended_8/unfolded_genes.pdf', width = 3.5, height = 4)
+pdf('../plots/Extended_7/unfolded_genes.pdf', width = 3.5, height = 4)
 barplot(x[order(x)], horiz = T, las = 1)
 dev.off()
 
