39c390d60e42910a7a0f1efb5f43e21b6ddf6d76
Djuna von Maydell <41922169+djunamay@users.noreply.github.com>
2022-10-30 13:26:35 -0400

    Update make_metadata_file.r

--- scripts/make_metadata_file.r
+++ scripts/make_metadata_file.r
@@ -1,5 +1,6 @@
-
+print('making metadata file')
 all_metadata = read.csv('../data/single_cell_data/qc_counts_data/qc_column_metadata.csv')
 meta = all_metadata[!duplicated(all_metadata$projid),]
 meta = meta[,c('projid', 'batch', 'amyloid', 'nft', 'msex', 'age_death','pmi', 'AD','apoe_genotype' ,'APOE4')]
 write.csv(meta, '../data/single_cell_data/metadata_by_individual.csv')
+print('done.')
