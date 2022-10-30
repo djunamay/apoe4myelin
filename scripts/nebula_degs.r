6a96f966b609f7127238ef84ac320e42e6c45bd0
Djuna von Maydell <41922169+djunamay@users.noreply.github.com>
2022-10-30 13:31:30 -0400

    adding print statements

--- scripts/nebula_degs.r
+++ scripts/nebula_degs.r
@@ -1,9 +1,12 @@
 ##############################################################################################################
 library(nebula)
+print('loading sce')
 sce = readRDS('../data/single_cell_data/single_cell_experiment_object.rds')
+print('subsetting oli')
 sce.oli <- sce[,sce$cell.type%in%"Oli"]
 gc()
 ##############################################################################################################
+print('setting up model')
 C <- counts(sce.oli)
 Predictors <- as.data.frame(sce.oli@colData[,c("amyloid","nft", "APOE4", "age_death", "batch")])
 Predictors$amyloid <- scale(Predictors$amyloid)
@@ -12,10 +15,12 @@ Predictors$age_death <- scale(Predictors$age_death)
 Predictors$APOE4 <- as.character(Predictors$APOE4)
 Predictors$batch <- as.character(Predictors$batch)
 df = model.matrix(~APOE4+amyloid+nft+age_death+batch, data=Predictors)
+print('computing degs')
 re = nebula(C, sce.oli$projid, pred=df, offset=Matrix::colSums(C))
 ##############################################################################################################
 out <- re$summary
 rownames(out) <- out$gene
 out$score <- -log10(DEoutList[[i]]$p_APOE4yes) * ifelse(DEoutList[[i]]$logFC_APOE4yes>0, 1, -1)
-saveRDS(out, file="../APOE4_Oligo_project/data/E4_nebula_associations_Oli.rds")
+print('saving degs')
+saveRDS(out, file="../data/single_cell_data/E4_nebula_associations_Oli.rds")
 ##############################################################################################################
