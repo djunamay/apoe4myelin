664c47d00cb60657d0af15ee0f52ec29ede7996d
Djuna von Maydell <41922169+djunamay@users.noreply.github.com>
2022-10-30 16:33:56 -0400

    Update get_wilcox_myelin_plots.r

--- scripts/get_wilcox_myelin_plots.r
+++ scripts/get_wilcox_myelin_plots.r
@@ -3,7 +3,7 @@
 print('|| plotting myelin gene perturbations (post-mortem oligos) ||')
 
 # show the degs for myelin genes with and without AD
-ad = readRDS('../data/differentially_expressed_genes_data/oli_wilcox_results_AD.rds')
+ad = readRDS('../data/differentially_expressed_genes/oli_wilcox_results_AD.rds')
 myelination <- c("MYRF","MOG","PLP1","PLLP","MAG","OPALIN")
 gg = ad[myelination,]
 gg = gg[gg$padj<0.05,]
@@ -15,7 +15,7 @@ barplot(ggg[order(ggg, decreasing = T)], las = 2, horiz = T, main = 'AD')
 dev.off()
 
 # show the degs for myelin genes with and without AD
-no = readRDS('../data/differentially_expressed_genes_data/oli_wilcox_results_noAD.rds')
+no = readRDS('../data/differentially_expressed_genes/oli_wilcox_results_noAD.rds')
 myelination <- c("MYRF","MOG","PLP1","PLLP","MAG","OPALIN")
 gg = no[myelination,]
 gg = gg[gg$padj<0.05,]
