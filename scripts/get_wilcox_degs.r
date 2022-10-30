045d1de55b384142e71725210a1919bd1a084812
Djuna von Maydell <41922169+djunamay@users.noreply.github.com>
2022-10-30 15:24:16 -0400

    ad yes no

--- scripts/get_wilcox_degs.r
+++ scripts/get_wilcox_degs.r
@@ -14,10 +14,10 @@ print('computing degs')
 out_all = Wilcox.differential(logcounts(oli), ifelse(oli$apoe_genotype==33, 0, 1))
 
 # stratified by pathology
-oli_ad = oli[,oli$niareagansc%in%c(1,2) & oli$apoe_genotype!=44]
+oli_ad = oli[,oli$AD=='yes' & oli$apoe_genotype!=44]
 out_ad = Wilcox.differential(logcounts(oli_ad), ifelse(oli_ad$apoe_genotype==33, 0, 1))
 
-oli_nonad = oli[,oli$niareagansc%in%c(3,4) & oli$apoe_genotype!=44]
+oli_nonad = oli[,oli$AD=='no' & oli$apoe_genotype!=44]
 out_nonad = Wilcox.differential(logcounts(oli_nonad), ifelse(oli_nonad$apoe_genotype==33, 0, 1))
 
 saveRDS(out_all, '../data/differentially_expressed_genes/oli_wilcox_results.rds')
